141a97f34924d60dd9728eeedd1dea224c4bc0e9
Justin Zhang <schnell18@gmail.com>
2020-03-08 15:30:22 +0800

    Add python code to access cassandra

---
+++ kubernetes/labs/cassandra/employee/python/test-insert.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from cassandra.cluster import Cluster
+
+if __name__ == '__main__':
+    cluster = Cluster(
+        contact_points=[
+            'slave-1.kube.vn',
+            'slave-2.kube.vn',
+            'slave-3.kube.vn'
+        ],
+        port=30844
+    )
+    session = cluster.connect('employee')
+    create_table = '''
+    create table python_test(
+        id uuid primary key,
+        first_name text,
+        last_name text
+    )
+    '''
+    insert = '''
+    insert into python_test(id, first_name, last_name)
+    values (uuid(), %s, %s)
+    '''
+    names = [('bob', 'dylan'), ('jack', 'sparrow'), ('jason', 'boune')]
+    for name in names:
+        session.execute(insert, name)
+    print("Finished")
+
+# vim: set ai nu nobk expandtab ts=4 sw=4 tw=80 syntax=python:
---
+++ kubernetes/labs/cassandra/employee/python/test-select.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from cassandra.cluster import Cluster
+
+if __name__ == '__main__':
+    cluster = Cluster(
+        contact_points=[
+            'slave-1.kube.vn',
+            'slave-2.kube.vn',
+            'slave-3.kube.vn'
+        ],
+        port=30844
+    )
+    session = cluster.connect('employee')
+    cql = '''
+    select * from python_test
+    '''
+    rows = session.execute(cql)
+    for row in rows:
+        print("%s %s" % (row.first_name, row.last_name))
+    print("Finished")
+
+# vim: set ai nu nobk expandtab ts=4 sw=4 tw=80 syntax=python:
