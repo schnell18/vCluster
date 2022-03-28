06082a353e0e6a0a29161bd83b7376d2f24d53d8
Justin Zhang <schnell18@gmail.com>
2022-03-28 09:00:22 +0800

    Reduce security risk

--- kubernetes/labs/operators/vistor/docker/visitors-service/visitors/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-
-from visitors.service import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('visitors/', views.VisitorAPI.as_view()),
-]
