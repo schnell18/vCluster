42ec619aaa64cf16bfc6e3184b65e3ca568c7f08
Justin Zhang <schnell18@gmail.com>
2022-04-17 18:29:09 +0800

    Delete vulnerable operator code

--- kubernetes/labs/operators/vistor/go/visitor-operator/pkg/apis/tinkerit/group.go
+++
@@ -1,6 +0,0 @@
-// Package tinkerit contains tinkerit API versions.
-//
-// This file ensures Go source parsers acknowledge the tinkerit package
-// and any child packages. It can be removed if any other Go source files are
-// added to this package.
-package tinkerit
--- kubernetes/labs/operators/vistor/go/visitor-operator/pkg/apis/tinkerit/v1/doc.go
+++
@@ -1,4 +0,0 @@
-// Package v1 contains API Schema definitions for the tinkerit v1 API group
-// +k8s:deepcopy-gen=package,register
-// +groupName=tinkerit.cf
-package v1
